ce25f9ff6521739b2844121968cf21dc993f0895
yanli1127 <yanli1127@163.com>
2020-09-02 19:23:36 +0800

    第一次测试提交

---
+++ src/test/java/com/cs/yl/springallinfo/SpringallinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs.yl.springallinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringallinfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
